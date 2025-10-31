5286182abd43cd24e4cf7fe8670ba8ddcc3af451
arielmezibrice-ui <arielmezibrice@gmail.com>
2025-10-31 11:01:29 +0100

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,3 +1,10 @@
 // main.js
 console.log("Application 'Mon avenir entre mes mains' chargée avec succès !");
 alert("Bienvenue dans l’application Mon avenir entre mes mains !");
+document.addEventListener('DOMContentLoaded', () => {
+    const bouton = document.querySelector('button');
+
+    bouton.addEventListener('click', () => {
+        alert('À venir : modules de formation, quiz et accompagnement personnalisé !');
+    });
+});
